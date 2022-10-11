7d5879f73905d780ff65d77b511c7d64f9e2a4b6
jjav <razornice@gmail.com>
2022-10-11 19:19:32 -0300

    game_over? for rows

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -15,13 +15,13 @@ class Board
       puts row.join("I")
       puts "====="
     end
-    #gameover? gameover: 
+    game_over if game_over?
   end
 
   def play(input_row, input_colum)
     board[input_row][input_colum] = player
     print_board
-    switch_player
+    switch_player unless game_over?
   end
 
   def switch_player
@@ -37,6 +37,7 @@ class Board
 
   def box_select
     puts "inser two digits. Both 1 to 3"
+    #add restriction for number ouside range o nonnumbers
     input = gets.chomp
     puts "You selected #{input}"
     input_row = input[0].to_i-1
@@ -44,9 +45,17 @@ class Board
     play(input_row, input_colum)
   end
 
+  def game_over?
+    board.reject{|row|row.include?(" ")}.any?{ |row| row == ([row.first] * row.count)}
+  end
+
+  def game_over
+    puts "GAME OVER! Player #{player.upcase} won!"
+  end
 end
 
 my_board =  Board.new
-my_board.print_board
 my_board.box_select
 
+# ex_arr = [['x', ' ', ' '],['x', ' ', ' '],['x', '', ' ']]
+# ex_arr[0].intersection(ex_arr[1], ex_arr[2])
